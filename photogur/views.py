abef13a08f132b2816fc39143372201e8141b9bb
Sanchit Jain <sanchit.jain@mail.mcgill.ca>
2019-03-03 19:02:21 -0500

    Blog post functionality

---
+++ photogur/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from photogur.models import Picture, Comment
+
+def root(request):
+    return HttpResponseRedirect('pictures')
+
+# def home(request):
+#     return HttpResponseRedirect('pictures')
+
+def pictures(request):
+    context = {'pictures': Picture.objects.all()}
+    response = render(request, 'pictures.html', context)
+    return HttpResponse(response)
